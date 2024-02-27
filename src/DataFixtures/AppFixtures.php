cc0000854896f233461f999ab83c35ee0a534b7e
selena <selena.fahet.kestis@gmail.com>
2024-02-27 22:41:24 +0100

    ajout deuxième table

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Crud;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\User;
@@ -12,11 +13,22 @@ class AppFixtures extends Fixture
     {
         // Créer un utilisateur de test
         $user = new User();
-        $user->setUsername('test_user');
-        $user->setEmail('test@example.com');
-        $user->setPassword('test_password');
+        $user->setMail('test@example.com');
+        $user->setMdp('test_password');
+        $user->setNom('test_user');
+        $user->setPrenom('Test');
 
         $manager->persist($user);
         $manager->flush();
+
+        // Créer une entité Crud de test
+        $crud = new Crud();
+        $crud->setNom('test_nom');
+        $crud->setPrenom('test_prenom');
+        $crud->setDate(new \DateTime('now'));
+
+        $manager->persist($crud);
+        $manager->flush();
     }
+
 }
